cde4668dc07ad3e66e06b0c9c30867e56b117f79
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-01 02:28:08 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,16 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if !params[:artist_id].blank?
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist == nil
+        redirect_to artists_path
+      else
+        @song = Song.new(artist_id: params[:artist_id])
+      end
+    else
+      @song = Song.new
+    end
   end
 
   def create
